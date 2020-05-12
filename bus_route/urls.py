f11b71644692b504af1d576aa5612e5edb5de9b9
Khairul Islam <whoiskhairul@gmail.com>
2020-05-13 02:20:46 +0600

    khairul

--- bus_route/urls.py
+++ bus_route/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from user_info import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('khairul/', admin.site.urls),
     path('login',views.user_login,name='login'),
     path('bus/',include('user_info.urls'),name='user'),
     path('map/',include('map.urls'),name='map'),
